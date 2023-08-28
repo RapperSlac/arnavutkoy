ce317f323dc3f6b128fa2f0ecefcf35333b30efd
sudekpln <skgba1907@gmail.com>
2023-08-28 12:47:34 +0300

    karakter

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,10 +10,14 @@ public class Character : MonoBehaviour
     private bool canJumpInAir = false; // Havada iken zýplamaya izin verme kontrolü
 
     private Rigidbody rb;
+    private Animator animator;
+    public float rotationSpeed = 90f;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
 
@@ -23,30 +27,25 @@ public class Character : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(new Vector3(0, 0, 5) * Time.deltaTime);
+            animator.SetBool("isW", true);
+            transform.Translate(new Vector3(0, 0, 7) * Time.deltaTime);
         }
 
-
-        if (Input.GetKey(KeyCode.D))
+        else
         {
-            transform.Translate(new Vector3(2, 0, 0) * Time.deltaTime);
-        }
-
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(new Vector3(-2, 0, 0) * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(new Vector3(0, 0, -5) * Time.deltaTime);
+            animator.SetBool("isW", false);
         }
 
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
         {
-             transform.Translate(new Vector3(0, 0, 40) * Time.deltaTime);
+            animator.SetBool("isWandShift", true);
+            transform.Translate(new Vector3(0, 0, 15) * Time.deltaTime);
         }
+        else
+        {
+            animator.SetBool("isWandShift", false);
+        }
+
 
         if (isGrounded)
         {
@@ -55,8 +54,53 @@ public class Character : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || canJumpInAir))
         {
+            animator.SetBool("isSpace", true);
             Jump();
         }
+
+        else
+        {
+            animator.SetBool("isSpace", false);
+        }
+
+
+
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rotated();
+        }
+
+        void Rotated()
+        {
+            float rotationAmount = rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up * rotationAmount); // Nesneyi y ekseninde döndürme
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rotatea();
+        }
+
+        void Rotatea()
+        {
+            float rotationAmount = rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up * -rotationAmount); // Nesneyi y ekseninde döndürme
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("isWr", true);
+            transform.Translate(new Vector3(0, 0, -5) * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("isWr", false);
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
